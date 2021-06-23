d448552eb1e72fd5bc44f0c7bd96bbdd00428cff
Sonal <Sonal@LAPTOP-6KB6GK8H>
2021-06-23 10:18:42 +0530

    Zeyo Project Created

---
+++ SparkZeyoGitN/src/main/scala/SparkZeyoGit/SparkZeyoGit/App.java
@@ -0,0 +1,13 @@
+package SparkZeyoGit.SparkZeyoGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
